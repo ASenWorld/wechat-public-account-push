d5dd09e6c09ec832827c9115bbe51752adc5f0ab
Asen <76636191+ASenWorld@users.noreply.github.com>
2024-04-01 20:36:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oAaGV6vG_ZYTctQYQSZjBgwvT2yA',
+      id: 'oAaGV6ucBfKog8pKlYL6ZgV9qJN0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '3Bt8dcE3oMb3Rf3RiEtzL3lKuA_50FFslr22SRjauzM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
