c0eab7610a8b5d6ecfa5f77bf469cb6f13789acd
Asen <76636191+ASenWorld@users.noreply.github.com>
2024-04-01 20:12:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'f05410345d0b99c2e205be14c526bf83',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '江西',
+  CITY: '南昌',
 
   USERS: [
     {
